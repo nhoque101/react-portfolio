01720fbf428987051c3951423ebb0feed418c7fe
nhoque101 <naimul991@gmail.com>
2025-01-03 14:27:06 -0500

    picture added, learned css vairiables and usage

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA.jsx'
+import ME from '../../assets/mee.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5> Hello I'm</h5>
+        <h1>Naimul Hoque</h1>
+        <h5 className="text-light">Full-stack Developer</h5>
+
+        <CTA />
+
+        <div className="me">
+            <img src={ME} alt=""/>
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+
+
+      </div>
+
+    </header>
   )
 }
 
